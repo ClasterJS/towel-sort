4bce99b3818377138b87b0175839959a04c1bd7d
Alex Chikun <Claster85@gmail.com>
2020-10-05 13:45:12 +0300

    fix: add check for empty array & no param passed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-
-    let newArr = matrix;
-    for (let i = 0; i < newArr.length; i++) {
-      if (i % 2 !== 0) {
-        newArr[i].sort((a , b) => b - a);
-      }
-    }
-    return newArr.flat();
+        if (arguments.length === 0) {
+            return []
+        } else {
+            let newArr = matrix;
+            for (let i = 0; i < newArr.length; i++) {
+            if (i % 2 !== 0) {
+                newArr[i].sort((a , b) => b - a);
+            }
+            }
+            return newArr.flat();
+        }
 }
